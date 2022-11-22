e2224a8f569150934e4179e55d476cbc0644cbb9
Nikan <njavadzadeh@gmail.com>
2022-11-22 15:40:10 -0800

    commit tree b0dbde1d058dbf0cb01e634c00473db9502fcba1

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(numba)
+    while numba>0
+        puts "#{numba} SECOND(S)!"
+    numba-=1
+    end
+ "HAPPY NEW YEAR!"  
+end
+
+
+def countdown_with_sleep(numba)
+sleep 5
+while numba>0
+    puts "#{numba} SECOND(S)!"
+numba-=1
+end
+"HAPPY NEW YEAR!"  
 end
